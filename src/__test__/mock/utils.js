45ac6653de90f1aae106eaf9cf43e2784e27613f
Sama Gyaneshwar <sama.gyaneshwar@sourcefuse.com>
2022-06-24 12:11:05 +0530

    feat: add test utils
    - mockResponse
    - mockApi
    - mockWait

--- src/__test__/mock/utils.js
+++ src/__test__/mock/utils.js
@@ -1,3 +1,4 @@
+import moxios from "moxios";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
@@ -5,9 +6,28 @@ import { postsInitialState } from "../../redux/slices/postsSlice";
 import { posts } from "./data";
 
 export const WrapProvider = (OriginalComponent) => {
-  const mockStore = configureStore([thunk])({
-    posts: {...postsInitialState, posts: posts},
+/**
+ * 
+ * @param {object} data { status: number, response: object } 
+ */
+export const mockResponse = (data) => {
+  return moxios.wait(() => {
+    const request = moxios.requests.mostRecent();
+    request.respondWith(data);
   });
-
-  return <Provider store={mockStore}>{OriginalComponent}</Provider>;
 };
+
+/**
+ * 
+ * @param {string} url api url 
+ * @param {*} data { status: number, response: object } 
+ */
+export const mockApi = (url, data) => {
+  return moxios.stubRequest(url, data);
+}
+
+export const mockWait = (cb, timeout = 0) => {
+  moxios.wait(() => {
+    cb();
+  }, timeout)
+}
